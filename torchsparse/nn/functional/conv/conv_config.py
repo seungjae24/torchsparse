b7a050dfd0720e174ba06c65024b70ee9d3f56ab
seungjae <sj98lee@gmail.com>
2025-02-03 09:11:39 +0900

    hashmap as a default config mode

--- torchsparse/nn/functional/conv/conv_config.py
+++ torchsparse/nn/functional/conv/conv_config.py
@@ -16,7 +16,7 @@ _default_conv_config = AttributeDict(
     [
         ("dataflow", Dataflow.ImplicitGEMM),
         ("ifsort", False),
-        ("kmap_mode", "hashmap_on_the_fly"),
+        ("kmap_mode", "hashmap"),
         ("downsample_mode", "spconv"),
         ("split_mask_num", 1),
         ("split_mask_num_bwd", 3),
